dd94b22798877e2e28149504a26e7375579e3b63
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-03-11 22:14:20 +0600

    Create Parallax and Add Testimonials with react rating

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,7 +4,7 @@ import "./Featured.css"
 
 const Featured = () => {
   return (
-    <div className="featured-bg-img text-white pt-8 my-20">
+    <div className="featured-bg-img bg-fixed text-white pt-8 my-20">
       <SectionTitle
         heading={"Featured Items"}
         subHeading={"Explore Our Culinary Masterpieces"}
@@ -18,7 +18,7 @@ const Featured = () => {
           <p>11 March, 2025</p>
           <p className="uppercase">Our Featured Delicacies</p>
           <p>At Food Heaven, we take pride in serving dishes that not only satisfy your cravings but also leave a lasting impression on your taste buds. Our featured items are crafted with the freshest ingredients and a passion for perfection, bringing you an unforgettable dining experience. From mouthwatering appetizers to delightful main courses and decadent desserts, each dish is a masterpiece prepared with love and care. Indulge in our signature creations and discover why Food Heaven is the ultimate destination for food enthusiasts!</p>
-          <button className="btn btn-outline text-white">Order Now</button>
+          <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">Order Now</button>
         </div>
       </div>
     </div>
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,44 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
+  console.log(reviews);
+
+  return (
+    <section className="my-16">
+      <SectionTitle
+        heading="Testimonials"
+        subHeading="Words That Warm Our Hearts!"
+      ></SectionTitle>
+
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="mx-20 flex flex-col items-center">
+              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <i className="fa-solid fa-quote-left text-5xl pt-5"></i>
+              <p className="py-3">{review.details}</p>
+              <h3 className="text-2xl text-orange-500">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
