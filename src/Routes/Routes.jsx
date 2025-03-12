d0ca1c691585a4d318ca4840bdf3c8853d5f53dd
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-03-12 16:03:59 +0600

    Dynamic Page Title By Route Using React Helmet

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import {
 } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../pages/Home/Home/Home";
+import Menu from "../pages/Menu/Menu/Menu";
 
 
 
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>
 
+      },
+      {
+        path: "menu",
+        element: <Menu></Menu>
       }
     ]
   },
---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import { Helmet} from 'react-helmet-async';
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>FoodHeaven | Menu</title>
+      </Helmet>
+
+      <h1>this is menu page</h1>
+    </div>
+  );
+};
+
+export default Menu;
