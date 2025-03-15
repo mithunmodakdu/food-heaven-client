f3eba2c9850620fcdf8b36b729a746bbb6c99855
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-03-15 23:55:50 +0600

    Select right tab based on the category

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -8,9 +8,12 @@ import OrderTab from "../OrderTab/OrderTab";
 import { useParams } from "react-router-dom";
 
 const Order = () => {
-  const [tabIndex, setTabIndex] = useState(0);
-  const category = useParams();
-  console.log(category)
+  const categories = ['salad', 'pizza', 'soup', 'desserts', 'drinks'];
+  const {category} = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  
+  
 
   const [menu] = useMenu();
   
