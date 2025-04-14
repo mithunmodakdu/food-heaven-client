be95e6d3f09d9bfa4d5d1e00bfb324ecf250e72e
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-14 13:12:23 +0600

    axios interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,32 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+  // request interceptor to  add authorization headers for every secure call to the api
+  axiosSecure.interceptors.request.use(
+    function(config){
+     const token = localStorage.getItem('access-token');
+     config.headers.authorization = `Bearer ${token}`;
+     return config;
+    }, 
+
+    function(error){
+     return Promise.reject(error);
+    }
+  )
+
+  // response interceptor for 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function(response){
+      return response;
+    },
+
+    (error) =>{
+      console.log('error in the interceptor', error)
+      const status = error.response.status;
+      return Promise.reject(error);
+    }
+  )
+
   return axiosSecure;
 };
 
