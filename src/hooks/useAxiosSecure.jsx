5f0fdca02a17d182c4aed0c8762940e89d62ee44
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-15 21:34:53 +0600

    Interceptors have been completed with Logout unauthorized access

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,35 +1,44 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuthInfo from "./useAuthInfo";
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000'
-})
+  baseURL: "http://localhost:5000",
+});
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const {logOut} = useAuthInfo();
   // request interceptor to  add authorization headers for every secure call to the api
   axiosSecure.interceptors.request.use(
-    function(config){
-     const token = localStorage.getItem('access-token');
-     config.headers.authorization = `Bearer ${token}`;
-     return config;
-    }, 
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
 
-    function(error){
-     return Promise.reject(error);
+    function (error) {
+      return Promise.reject(error);
     }
-  )
+  );
 
   // response interceptor for 401 and 403 status
   axiosSecure.interceptors.response.use(
-    function(response){
+    function (response) {
       return response;
     },
 
-    (error) =>{
-      console.log('error in the interceptor', error)
+    async (error) => {
+      console.log("error in the interceptor", error);
+      // for 401 and 403 status logout the user and move to the login page
       const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
-  )
+  );
 
   return axiosSecure;
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -72,7 +72,7 @@ const AuthProvider = ({ children }) => {
     return () => {
       return unsubscribe();
     };
-  }, []);
+  }, [axiosPublic]);
 
   const authInfo = {
     user,
