82a5ef6cc384d9b6a74e123db4555b278e63ab5c
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-24 21:58:50 +0600

    cart dropdown and user dropdown in navbar have been completed

--- src/pages/Shared/NavBar/Navbar.jsx
+++ src/pages/Shared/NavBar/Navbar.jsx
@@ -8,9 +8,11 @@ import useAdmin from "../../../hooks/useAdmin";
 
 const Navbar = () => {
   // const { user, logOut } = useContext(AuthContext);
-  const {user, logOut} = useAuthInfo();
+  const { user, logOut } = useAuthInfo();
+  console.log(user);
   const [isAdmin] = useAdmin();
   const [carts] = useCart();
+  const totalPrice = carts.reduce((total, item) => total + item.price, 0);
 
   const handleLogOut = () => {
     logOut()
@@ -25,38 +27,10 @@ const Navbar = () => {
       </li>
       <li>
         <Link to="/menu">Our Menu</Link>
-      </li> 
+      </li>
       <li>
         <Link to="/order/salad">Order</Link>
       </li>
-      {
-        user && isAdmin && <li><Link to="/dashboard/adminHome">Dashboard</Link></li>
-      }
-      {
-        user && !isAdmin && <li><Link to="/dashboard/userHome">Dashboard</Link></li>
-      }
-      <li>
-        <Link to="/dashboard/cart">
-          <button className="btn">
-            <FaCartShopping className="text-2xl" />
-            <div className="badge badge-secondary">+{carts.length}</div>
-          </button>
-        </Link>
-      </li>
-
-      {user ? (
-        <>
-          <button onClick={handleLogOut} className="btn btn-ghost">
-            Log out
-          </button>
-        </>
-      ) : (
-        <>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </>
-      )}
     </>
   );
 
@@ -100,7 +74,91 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <div className="flex gap-6">
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+              >
+                <div className="indicator">
+                  <FaCartShopping className="text-4xl  text-white" />
+                  <span className="badge badge-lg indicator-item bg-[#F47D2C] p-2">
+                    + {carts.length}
+                  </span>
+                </div>
+              </div>
+              <div
+                tabIndex={0}
+                className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+              >
+                <div className="card-body">
+                  <span className="text-lg font-bold text-[#F47D2C]">
+                    {carts.length} Items
+                  </span>
+                  <span className="text-[#F47D2C]">
+                    Total Price: ${totalPrice}
+                  </span>
+                  <div className="card-actions">
+                    <Link to="/dashboard/cart">
+                      <button className="btn bg-[#F47D2C] btn-block">
+                        View cart
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              {user ? (
+                <>
+                  <div className="dropdown dropdown-end ">
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn btn-ghost btn-circle avatar"
+                    >
+                      <div className="w-12 rounded-full">
+                        <img alt="User Image" src={user?.photoURL} />
+                      </div>
+                    </div>
+                    <ul
+                      tabIndex={0}
+                      className="menu menu-sm dropdown-content bg-[#F47D2C]  rounded-box z-1 mt-3 w-52 p-2 shadow"
+                    >
+                      <h2 className="text-xl">{user?.displayName}</h2>
+
+                      {user && isAdmin && (
+                        <li>
+                          <Link to="/dashboard/adminHome">Dashboard</Link>
+                        </li>
+                      )}
+                      {user && !isAdmin && (
+                        <li>
+                          <Link to="/dashboard/userHome">Dashboard</Link>
+                        </li>
+                      )}
+
+                      <li>
+                        <button
+                          onClick={handleLogOut}
+                          className="btn btn-ghost"
+                        >
+                          Log out
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                </>
+              ) : (
+                <>
+                  
+                  <Link to="/login">Login</Link>
+                  
+                </>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
