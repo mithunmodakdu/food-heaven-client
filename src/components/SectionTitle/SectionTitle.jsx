1f6b192564d42bb7367be51f807aef5a23062ae0
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-06-24 11:54:48 +0600

    Featured part has been updated

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,8 +1,8 @@
-const SectionTitle = ({heading, subHeading}) => {
+const SectionTitle = ({heading, subHeading, isFeatured}) => {
   return (
     <div className="w-3/4 md:w-4/12 lg:w-3/12 mx-auto text-center mt-[70px] mb-[30px]">
       <p className="text-primaryColor mb-2"><i className="fa-solid fa-burger"></i> <i className="fa-solid fa-mug-hot"></i> {subHeading}</p>
-      <h3 className="text-2xl md:text-3xl text-headingColor font-heading uppercase border-b-2 pb-2">{heading}</h3>
+      <h3 className={`text-2xl md:text-3xl ${isFeatured? 'text-white' : 'text-headingColor'}  font-heading uppercase border-b-2 pb-2`}>{heading}</h3>
     </div>
   );
 };
--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,24 +1,40 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import featuredImg from "../../../assets/home/featured.jpg";
-import "./Featured.css"
+import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div className="featured-bg-img bg-fixed text-white pt-8 my-20">
-      <SectionTitle
-        heading={"Featured Items"}
-        subHeading={"Explore Our Culinary Masterpieces"}
-      ></SectionTitle>
+    <div className="hero featured-bg-img bg-fixed text-white my-20">
+      <div className="hero-overlay"></div>
+      <div className="">
+        <SectionTitle
+          heading={"Featured Items"}
+          subHeading={"Explore Our Culinary Masterpieces"}
+          isFeatured = {true}
+        ></SectionTitle>
 
-      <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
-        <div>
-          <img src={featuredImg} alt="" />
-        </div>
-        <div className="md:ml-10">
-          <p>11 March, 2025</p>
-          <p className="uppercase">Our Featured Delicacies</p>
-          <p>At Food Heaven, we take pride in serving dishes that not only satisfy your cravings but also leave a lasting impression on your taste buds. Our featured items are crafted with the freshest ingredients and a passion for perfection, bringing you an unforgettable dining experience. From mouthwatering appetizers to delightful main courses and decadent desserts, each dish is a masterpiece prepared with love and care. Indulge in our signature creations and discover why Food Heaven is the ultimate destination for food enthusiasts!</p>
-          <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">Order Now</button>
+        <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+          <div>
+            <img src={featuredImg} alt="" />
+          </div>
+          <div className="md:ml-10">
+            <p>11 March, 2025</p>
+            <p className="uppercase">Our Featured Delicacies</p>
+            <p>
+              At Food Heaven, we take pride in serving dishes that not only
+              satisfy your cravings but also leave a lasting impression on your
+              taste buds. Our featured items are crafted with the freshest
+              ingredients and a passion for perfection, bringing you an
+              unforgettable dining experience. From mouthwatering appetizers to
+              delightful main courses and decadent desserts, each dish is a
+              masterpiece prepared with love and care. Indulge in our signature
+              creations and discover why Food Heaven is the ultimate destination
+              for food enthusiasts!
+            </p>
+            <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">
+              Order Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
